e9993cfb7c1946e115ae211834d7bafa7e9ae394
sohelhussain <sohelhussaing@gmail.com>
2025-06-02 09:14:33 +0530

    My neo vim

---
+++ lua/plugins/smear_cursor.lua
@@ -0,0 +1,12 @@
+return {
+  'sphamba/smear-cursor.nvim',
+  event = 'VeryLazy',
+  opts = {
+    smear_between_buffers = true,
+    smear_between_neighbor_lines = true,
+    scroll_buffer_space = true,
+    legacy_computing_symbols_support = false,
+    smear_insert_mode = true,
+    cursor_color = '#d3cdc3', -- optional: set to your cursor color if needed
+  },
+}
